499043ec42f27e67bd32e33f01959f2b389209ff
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-07-31 14:17:31 +0300

    Add CollctinoView & ProductCell
    Add CollectionView to the app,
    and Add ProductCell to the View folder.

---
+++ Coder Shop/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  Coder Shop
+//
+//  Created by Hasan Elhussein on 31.07.2021.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
