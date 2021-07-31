78c90601440734e2e01b4ba0df9640c83883fbb8
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-07-31 12:23:25 +0300

    Add Table View & CategoryCell
    Add Table View to the application,
    and CategoryCell to the View folder.

---
+++ Coder Shop/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  Coder Shop
+//
+//  Created by Hasan Elhussein on 31.07.2021.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
